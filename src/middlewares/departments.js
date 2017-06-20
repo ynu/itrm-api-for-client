1f8f470c7fd28f1cccc0c13ff367b33735502300
liudonghua123 <liudonghua123@gmail.com>
2017-06-20 20:30:44 +0800

    update middlewares

--- src/middlewares/departments.js
+++ src/middlewares/departments.js
@@ -1,156 +1,39 @@
-const { ObjectId } = require('mongodb');
-const DepartmentManager = require('../models/departments').default;
+import DepartmentManger from '../models/departments';
+import {totalCount as totalCountCommon, list as listCommon,
+  getById as getByIdCommon, updateById as updateByIdCommon,
+  deleteById as deleteByIdCommon} from './crud';
 
-export const totalCount = (options = {}) => async (req, res, next) => {
-  const db = options.db;
-  const getQuery = options.getQuery || (req2 => req2.mongoQuery);
-  const getCurrentUserId = options.getCurrentUserId || (req2 => req2.user.id);
-  const success = options.success || ((count, req2, res2, next2) => {
-    req2.departments = {
-      ...req.departments,
-      totalCount: count,
-    };
-    next2();
-  });
-  const deptm = new DepartmentManager(db);
-  const queryOptions = getQuery(req);
-  const userId = getCurrentUserId(req);
-  const count = await deptm.count({
-    ...queryOptions.query,
-    $or: [
-      { 'zyfzr.id': userId },
-      { 'bmscy.id': userId },
-      { 'creation.creator.id': userId },
-    ],
-  });
-  success(count, req, res, next);
+const defaultOptions = {
+  entityManger: DepartmentManger,
+  dataName: 'departments',
+  whereQueryOrFields: ['zyfzr.id', 'bmscy.id', 'creation.creator.id'],
+  whereCheckOrFields: [['bmscy','id'], ['creation','creator','id']]
+}
+
+export const totalCount = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return totalCountCommon(mergedOptions);
 };
 
-export const list = (options = {}) => async (req, res, next) => {
-  const db = options.db;
-  const getQuery = options.getQuery || (req2 => req2.mongoQuery);
-  const getCurrentUserId = options.getCurrentUserId || (req2 => req2.user.id);
-  const success = options.success || ((data, req2, res2, next2) => {
-    req2.departments = {
-      ...req.departments,
-      list: data,
-    };
-    next2();
-  });
-  const deptm = new DepartmentManager(db);
-  const queryOptions = getQuery(req);
-  const userId = getCurrentUserId(req);
-  const data = await deptm.find({
-    ...queryOptions,
-    query: {
-      ...queryOptions.query,
-      $or: [
-      { 'zyfzr.id': userId },
-      { 'bmscy.id': userId },
-      { 'creation.creator.id': userId },
-      ],
-    },
-  });
-  console.log('data', data.length);
-  console.log(JSON.stringify({
-    ...queryOptions,
-    query: {
-      ...queryOptions.query,
-      $or: [
-      { 'zyfzr.id': userId },
-      { 'bmscy.id': userId },
-      { 'creation.creator.id': userId },
-      ],
-    },
-  }));
-  success(data, req, res, next);
+export const list = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return listCommon(mergedOptions);
 };
 
-export const getById = (options = {}) => async (req, res, next) => {
-  const db = options.db;
-  const getId = options.getId || (req2 => req2.params.id);
-  const getCurrentUserId = options.getCurrentUserId || (req2 => req2.user.id);
-  const success = options.success || ((data, req2, res2) => {
-    res2.json(data);
-  });
-  const fail = options.fail || ((err, req2, res2) => {
-    res2.status(403).send('当前用户没有权限');
-  });
-  const deptm = new DepartmentManager(db);
-  const id = new ObjectId(getId(req));
-  const userId = getCurrentUserId(req);
-  const data = await deptm.findById(id);
-  if (data && (
-    data.zyfzr.id === userId ||
-    data.bmscy.id === userId ||
-    data.creation.creator.id === userId
-   )) success(data, req, res, next);
-  else fail(new Error('当前用户没有权限'), req, res, next);
+export const getById = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return getByIdCommon(mergedOptions);
 };
 
-export const updateById = (options = {}) => async (req, res, next) => {
-  const db = options.db;
-  const getId = options.getId || (req2 => req2.params.id);
-  const getData = options.getData || (req2 => req2.body);
-  const getCurrentUserId = options.getCurrentUserId || (req2 => req2.user.id);
-  const success = options.success || ((id, req2, res2) => {
-    res2.json({ id });
-  });
-  const fail = options.fail || ((err, req2, res2) => {
-    res2.status(403).send('当前用户没有权限');
-  });
-  const deptm = new DepartmentManager(db);
-  const _id = new ObjectId(getId(req));
-  const userId = getCurrentUserId(req);
-  const newData = getData(req);
-  const data = await deptm.findById(_id);
-  if (data && (
-    data.bmscy.id === userId ||
-    data.creation.creator.id === userId
-   )) {
-    await deptm.updateById({
-      ...newData,
-      _id,
-    });
-    success(_id, req, res, next);
-  } else fail(new Error('当前用户没有权限'), req, res, next);
+export const updateById = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return updateByIdCommon(mergedOptions);
 };
 
-export const deleteById = (options = {}) => async (req, res, next) => {
-  const db = options.db;
-  const getId = options.getId || (req2 => req2.params.id);
-  const getCurrentUserId = options.getCurrentUserId || (req2 => req2.user.id);
-  const success = options.success || ((id, req2, res2) => {
-    res2.json({ id });
-  });
-  const fail = options.fail || ((err, req2, res2) => {
-    res2.status(403).send('当前用户没有权限');
-  });
-  const deptm = new DepartmentManager(db);
-  const id = new ObjectId(getId(req));
-  const userId = getCurrentUserId(req);
-  const data = await deptm.findById(id);
-  if (data && (
-    data.bmscy.id === userId ||
-    data.creation.creator.id === userId
-   )) {
-    await deptm.removeById(id);
-    success(id, req, res, next);
-  } else fail(new Error('当前用户没有权限'), req, res, next);
+export const deleteById = (options = {}) => {
+  let mergedOptions = Object.assign({}, defaultOptions, options);
+  return deleteByIdCommon(mergedOptions);
 };
-
-// export const create = (options = {}) => (req, res, next) => {
-//   const db = options.db;
-//   const getData = options.getData || (req2 => req2.newData);
-//   const getCurrentUserId = options.getCurrentUserId || (req2 => req2.user.id);
-//   const success = options.success || ((insertedId, req2, res2, next2) => {
-//     req2.departments = {
-//       ...req.departments,
-//       insertedId,
-//     };
-//     next2();
-//   });
-// };
 
 export default {
   totalCount,
--- src/middlewares/wechat-official-accounts.js
+++ src/middlewares/wechat-official-accounts.js
@@ -1,19 +1,41 @@
-const WechatOfficialAccountManager = require('../models/wechat-official-accounts').default;
+import WechatOfficialAccountManager from '../models/wechat-official-accounts';
+import {totalCount as totalCountCommon, list as listCommon,
+  getById as getByIdCommon, updateById as updateByIdCommon,
+  deleteById as deleteByIdCommon} from './crud';
 
-export const totalCount = (options = {}) => async (req, res, next) => {
-  const db = options.db;
-  const success = options.success || ((count, req2, res2, next2) => {
-    req2.wechatOfficialAccounts = {
-      ...req.wechatOfficialAccounts,
-      totalCount: count,
-    };
-    next2();
-  });
-  const woam = new WechatOfficialAccountManager(db);
-  const count = await woam.count();
-  success(count, req, res, next);
+const defaultOptions = {
+  entityManger: WechatOfficialAccountManager,
+  dataName: 'wechatOfficialAccounts',
+  whereQueryOrFields: ['manager.id', 'creation.creator.id'],
+  whereCheckOrFields: [['manager','id'], ['creation','creator','id']]
+}
+
+export const totalCount = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return totalCountCommon(mergedOptions);
+};
+
+export const list = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return listCommon(mergedOptions);
+};
+
+export const getById = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return getByIdCommon(mergedOptions);
+};
+
+export const updateById = (options = {}) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  return updateByIdCommon(mergedOptions);
+};
+
+export const deleteById = (options = {}) => {
+  let mergedOptions = Object.assign({}, defaultOptions, options);
+  return deleteByIdCommon(mergedOptions);
 };
 
 export default {
   totalCount,
+  list,
 };
