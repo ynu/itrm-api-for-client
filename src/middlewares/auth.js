eef8fdcd06f67bd77c2798e13979850d82ba6bc2
na57 <na57@qq.com>
2017-06-20 15:21:25 +0800

    update: 修改验证失败后的默认处理方法

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,7 +8,7 @@ export const currentUser = (options = {}) => (req, res, next) => {
     };
     next2();
   });
-  const fail = options.fail || ((err, req2, res2, next2) => next2());
+  const fail = options.fail || ((err, req2, res2) => res2.status(401).send(err.message));
   info('current user: session', req.session.cas);
   if (req.session.cas && req.session.cas.user) success(req.session.cas.user, req, res, next);
   else fail(new Error('no user logged.'), req, res, next);
