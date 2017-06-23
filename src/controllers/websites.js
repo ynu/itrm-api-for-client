d2816ee54eaac1be86530b1e0164b96c6f304b23
na57 <na57@qq.com>
2017-06-23 14:31:41 +0800

    websites Create/GteList 权限验证

--- src/controllers/websites.js
+++ src/controllers/websites.js
@@ -3,12 +3,14 @@
 */
 
 import { Router } from 'express';
+import { ObjectId } from 'mongodb';
 // import { secret } from '../config';
 import { formatQuery, setContentRange } from '../middlewares/simple-rest';
+import { list, totalCount } from '../middlewares/websites';
 
-import { ObjectId } from 'mongodb';
 import WebSiteManager from '../models/websites';
 import { generateCreation } from '../middlewares/creation';
+import { currentUser } from '../middlewares/auth';
 
 export default (options) => {
   const { db, routeName } = options;
@@ -17,13 +19,34 @@ export default (options) => {
   const router = new Router();
 
   router.get('/',
+    currentUser({ db }),
     formatQuery(),
+    // 添加权限过滤
+    // 一条记录的查询条件仅限于：创建者、管理员。
+    (req, res, next) => {
+      const userId = req.user.id;
+      req.queryFilter = {
+        $or: [
+            { 'creation.creator.id': userId },
+            { 'manager.id': userId },
+        ],
+      };
+      next();
+    },
+    list({
+      db,
+      getFilter: req => req.queryFilter,
+    }),
+    totalCount({
+      db,
+      getFilter: req => req.queryFilter,
+    }),
     setContentRange({
       resource: routeName,
-      getCount: () => wsm.count(),
+      getCount: req => req.websites.totalCount,
     }),
     async (req, res) => {
-      const data = await wsm.find(req.mongoQuery);
+      const data = req.websites.list;
       res.json(data.map(({ _id, ...other }) => ({
         id: _id,
         ...other,
@@ -43,39 +66,18 @@ export default (options) => {
 
   // 创建数据
   router.post('/',
-    // 规整数据
-    (req, res, next) => {
-      let { mainUrlPage } = req.body;
-      const { domain } = req.body;
-      if (!mainUrlPage) mainUrlPage = `http://${domain}`;
-      req.website = {
-        ...req.body,
-        mainUrlPage,
-      };
-      next();
-    },
+    currentUser(),
     generateCreation(),
     async (req, res) => {
       const id = await wsm.insert({
         creation: req.creation,
-        ...req.website,
+        ...req.body,
       });
       res.json({ id });
     }
   );
 
   router.put('/:id',
-    // 规整数据
-    (req, res, next) => {
-      let { mainUrlPage } = req.body;
-      const { domain } = req.body;
-      if (!mainUrlPage) mainUrlPage = `http://${domain}`;
-      req.website = {
-        ...req.body,
-        mainUrlPage,
-      };
-      next();
-    },
     async (req, res) => {
       const _id = new ObjectId(req.params.id);
       await wsm.updateById({
