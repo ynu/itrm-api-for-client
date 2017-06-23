ff704c6a85d7a07770f36a2e44c98a5a823ce003
na57 <na57@qq.com>
2017-06-23 14:31:58 +0800

    wechatOA Create/GetList 权限验证

--- src/middlewares/wechat-official-accounts.js
+++ src/middlewares/wechat-official-accounts.js
@@ -1,29 +1,29 @@
 import WechatOfficialAccountManager from '../models/wechat-official-accounts';
-import {totalCount as totalCountCommon, list as listCommon,
+import { totalCount as totalCountCommon, list as listCommon,
   getById as getByIdCommon, updateById as updateByIdCommon,
-  deleteById as deleteByIdCommon} from './crud';
+  deleteById as deleteByIdCommon } from './crud';
 
 const defaultOptions = {
   entityManger: WechatOfficialAccountManager,
   dataName: 'wechatOfficialAccounts',
   whereQueryOrFields: ['manager.id', 'creation.creator.id'],
-  whereCheckOrFields: [['manager','id'], ['creation','creator','id']]
-}
-
-export const totalCount = (options = {}) => {
-  const mergedOptions = Object.assign({}, defaultOptions, options);
-  return totalCountCommon(mergedOptions);
+  whereCheckOrFields: [['manager', 'id'], ['creation', 'creator', 'id']],
 };
 
-export const list = (options = {}) => {
-  const mergedOptions = Object.assign({}, defaultOptions, options);
-  return listCommon(mergedOptions);
-};
+export const totalCount = (options = {}) => totalCountCommon({
+  ...defaultOptions,
+  ...options,
+});
 
-export const getById = (options = {}) => {
-  const mergedOptions = Object.assign({}, defaultOptions, options);
-  return getByIdCommon(mergedOptions);
-};
+export const list = (options = {}) => listCommon({
+  ...defaultOptions,
+  ...options,
+});
+
+export const getById = (options = {}) => getByIdCommon({
+  ...defaultOptions,
+  ...options,
+});
 
 export const updateById = (options = {}) => {
   const mergedOptions = Object.assign({}, defaultOptions, options);
@@ -31,7 +31,7 @@ export const updateById = (options = {}) => {
 };
 
 export const deleteById = (options = {}) => {
-  let mergedOptions = Object.assign({}, defaultOptions, options);
+  const mergedOptions = Object.assign({}, defaultOptions, options);
   return deleteByIdCommon(mergedOptions);
 };
 
